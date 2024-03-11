7aaa770735736e360bf6135dc7b68258cfe7a8d5
hanwldn76 <hanwldn76@gmail.com>
2024-03-11 13:32:53 +0900

    ch.5 commit

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -1,13 +1,18 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -33,5 +38,26 @@ public class MemberController {
         log.info(saved.toString());
         //System.out.println(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = "+id);
+        // 1. id를 조회해 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        // 3. 뷰 페이지 반환하기
+        return "/members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        // 1. id를 조회해 데이터 가져오기
+        ArrayList<Member> memberListEntity = memberRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("memberList", memberListEntity);
+        // 3. 뷰 페이지 반환하기
+        return "/members/index";
     }
 }
