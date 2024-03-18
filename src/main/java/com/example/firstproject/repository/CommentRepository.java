2d817f01a9a97e47844fb0f0f4ef92b21972c72a
hanwldn76 <hanwldn76@gmail.com>
2024-03-18 16:00:24 +0900

    ch.14 commit

---
+++ src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -0,0 +1,17 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // 특정 게시글의 모든 댓글 조회 (@Query 어노테이션 이용)
+    @Query(value = "select * from comment where article_id = :articleId",
+            nativeQuery = true)
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
+    // 특정 닉네임의 모든 댓글 조회 (orm.xml 파일 이용)
+    List<Comment> findByNickname(@Param("nickname") String nickname);
+}
